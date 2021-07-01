28522067a6a26766d950422fc58a4a830b100e02
Felipe Ascari <felipe.ascari@gmail.com.br>
2021-07-01 18:45:31 -0300

    Contexto de testes unitários

--- src/main/java/com/fascari/minhasfinancas/Application.java
+++ src/main/java/com/fascari/minhasfinancas/Application.java
@@ -1,26 +1,13 @@
 package com.fascari.minhasfinancas;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
-@EnableWebMvc
-public class Application implements WebMvcConfigurer {
+public class Application {
 
     public static void main(String[] args) {
         run(Application.class, args);
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("HEAD,GET,POST,PUT,DELETE,PATCH,OPTIONS".split(","))
-                .allowedHeaders("Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method".split(","));
-
     }
 }
